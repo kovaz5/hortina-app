250e3cf477dca0f2dac288eb100b6ad39b485394
Alejandro Vázquez Corral <alex.vazquez.97@gmail.com>
2025-11-02 11:31:32 +0100

    limpieza do proxecto de probas

--- app/src/main/java/com/alex/hortina/data/ApiResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.alex.hortina.data
-
-data class ApiResponse(
-    val success: Boolean,
-    val message: String
-
-)
--- app/src/main/java/com/alex/hortina/data/Usuario.kt
+++
@@ -1,9 +0,0 @@
-package com.alex.hortina.data
-
-import java.util.Date
-
-data class Usuario (
-    val nombre: String,
-    val email: String,
-    val contrasena: String
-)
